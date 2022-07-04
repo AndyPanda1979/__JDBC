5813f26831c6fba75da8e6d24ea80f86c9014e85
Andrei <andypanda197907@gmail.com>
2022-07-04 18:43:44 +0300

    Исправлено: 1) В Util.getConnection() не проверять на null, а всегда возвращать новое соединение. 2) В dao каждый метод должен брать новое соединение, работать с ним и закрывать. 3) В dao реализовать логику транзакций. 4) В методах с подстановкой значений в запрос использовать PreparedStatement

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,9 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,58 +13,133 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() throws SQLException {
-        String rawSQL = """
-                CREATE TABLE IF NOT EXISTS `test1`.`new_table` (
-                  `id` BIGINT(64) NOT NULL AUTO_INCREMENT,
-                  `name` VARCHAR(45) NOT NULL,
-                  `lastName` VARCHAR(45) NOT NULL,
-                  `age` INT NOT NULL,
-                  PRIMARY KEY (`id`));""";
-        try (Statement statement = Util.getConnection().createStatement()) {
-            statement.execute(rawSQL);
+        try (Connection currentConnection = Util.getConnection()) {
+            if (currentConnection != null) {
+                try {
+                    currentConnection.setAutoCommit(false);
+                    String rawSQL = """
+                        CREATE TABLE IF NOT EXISTS `test1`.`users` (
+                        `id` BIGINT(64) NOT NULL AUTO_INCREMENT,
+                        `name` VARCHAR(45) NOT NULL,
+                        `lastName` VARCHAR(45) NOT NULL,
+                        `age` INT NOT NULL,
+                        PRIMARY KEY (`id`));""";
+                    currentConnection.createStatement().executeUpdate(rawSQL);
+                    currentConnection.commit();
+                } catch (SQLException e) {
+                    System.out.println("Не удалось создать таблицу");
+                    e.printStackTrace();
+                    currentConnection.rollback();
+                    currentConnection.close();
+                }
+            } else {
+                System.out.println("Не удалось установить соединение с БД");
+            }
         }
     }
 
     public void dropUsersTable() throws SQLException {
-        String rawSQL = "DROP TABLE IF EXISTS `test1`.`new_table`;";
-        try (Statement statement = Util.getConnection().createStatement()) {
-            statement.execute(rawSQL);
+        try (Connection currentConnection = Util.getConnection()) {
+            if (currentConnection != null) {
+                try {
+                    currentConnection.setAutoCommit(false);
+                    String rawSQL = "DROP TABLE IF EXISTS `test1`.`users`;";
+                    currentConnection.createStatement().executeUpdate(rawSQL);
+                    currentConnection.commit();
+                } catch (SQLException e) {
+                    System.out.println("Не удалось удалить таблицу");
+                    currentConnection.rollback();
+                    currentConnection.close();
+                }
+            } else {
+                System.out.println("Не удалось установить соединение с БД");
+            }
         }
     }
 
     public void saveUser(String name, String lastName, byte age) throws SQLException {
-        String rawSQL = "INSERT INTO `test1`.`new_table` (`name`, `lastName`, `age`) VALUES ('" + name + "', '" + lastName + "', '" + age + "');";
-        try (Statement statement = Util.getConnection().createStatement()) {
-            statement.execute(rawSQL);
-            System.out.println("User с именем " + name + " " + lastName + " добавлен в БД");
+        try (Connection currentConnection = Util.getConnection()) {
+            if (currentConnection != null) {
+                try (PreparedStatement preparedStatement = currentConnection.prepareStatement("INSERT INTO `test1`.`users` (`name`, `lastName`, `age`) VALUES (?, ?, ?)")) {
+                    preparedStatement.setString(1, name);
+                    preparedStatement.setString(2, lastName);
+                    preparedStatement.setByte(3, age);
+                    currentConnection.setAutoCommit(false);
+                    preparedStatement.executeUpdate();
+                    currentConnection.commit();
+                    System.out.println("User с именем - " + name + " " + lastName + " добавлен в БД");
+                } catch (SQLException e) {
+                    System.out.println("Ошибка вставки записи, User не был добавлен");
+                    currentConnection.rollback();
+                    currentConnection.close();
+                }
+            } else {
+                System.out.println("Не удалось установить соединение с БД");
+            }
         }
     }
 
+
     public void removeUserById(long id) throws SQLException {
-        String rawSQL = "DELETE FROM `new_table` WHERE `id`=" + id + ";";
-        try (Statement statement = Util.getConnection().createStatement()) {
-            statement.execute(rawSQL);
+        try (Connection currentConnection = Util.getConnection()) {
+            if (currentConnection != null) {
+                try (PreparedStatement preparedStatement = currentConnection.prepareStatement("DELETE FROM `users` WHERE `id`=(?)")) {
+                    preparedStatement.setLong(1, id);
+                    currentConnection.setAutoCommit(false);
+                    preparedStatement.executeUpdate();
+                    currentConnection.commit();
+                } catch (SQLException e) {
+                    System.out.println("Ошибка удаления записи");
+                    currentConnection.rollback();
+                    currentConnection.close();
+                }
+            } else {
+                System.out.println("Не удалось установить соединение с БД");
+            }
         }
     }
 
     public List<User> getAllUsers() throws SQLException {
-        String rawSQL = "SELECT * FROM test1.new_table;";
-        List <User> users = new ArrayList<>();
-        try (Statement statement = Util.getConnection().createStatement()) {
-            ResultSet result = statement.executeQuery(rawSQL);
-            while (result.next()) {
-                User currentUser;
-                users.add(currentUser = new User(result.getString("name"), result.getString("lastName"), result.getByte("age")));
-                currentUser.setId(result.getLong("id"));
+        try (Connection currentConnection = Util.getConnection()) {
+            if (currentConnection != null) {
+                try (Statement statement = currentConnection.createStatement()) {
+                    String rawSQL = "SELECT * FROM test1.users;";
+                    currentConnection.setAutoCommit(false);
+                    ResultSet result = statement.executeQuery(rawSQL);
+                    currentConnection.commit();
+                    List<User> users = new ArrayList<>();
+                    while (result.next()) {
+                        User currentUser;
+                        users.add(currentUser = new User(result.getString("name"), result.getString("lastName"), result.getByte("age")));
+                        currentUser.setId(result.getLong("id"));
+                    }
+                    return users;
+                } catch (SQLException e) {
+                    System.out.println("Не удалось получить Users");
+                }
+            } else {
+                System.out.println("Не удалось установить соединение с БД");
             }
         }
-            return users;
+        return new ArrayList<>();
     }
 
     public void cleanUsersTable() throws SQLException {
-        String rawSQL = "TRUNCATE TABLE `new_table`;";
-        try (Statement statement = Util.getConnection().createStatement()) {
-            statement.execute(rawSQL);
+        try (Connection currentConnection = Util.getConnection()) {
+            if (currentConnection != null) {
+                try {
+                    currentConnection.setAutoCommit(false);
+                    String rawSQL = "TRUNCATE TABLE `users`;";
+                    currentConnection.createStatement().executeUpdate(rawSQL);
+                    currentConnection.commit();
+                } catch (SQLException e) {
+                    System.out.println("Не удалось очистить таблицу");
+                    currentConnection.rollback();
+                    currentConnection.close();
+                }
+            } else {
+                System.out.println("Не удалось установить соединение с БД");
+            }
         }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,19 +7,13 @@ import java.sql.SQLException;
 public class Util {
     // реализуйте настройку соеденения с БД
 
-    private static Connection connection;
-    public static Connection getConnection() throws SQLException {
+//    private static Connection connection;
+    public static Connection getConnection() {
         try {
-            if (connection == null) {
-                connection = DriverManager.getConnection("jdbc:mysql://localhost/test1", "root", "admin123");
-            }
-            return connection;
+            return DriverManager.getConnection("jdbc:mysql://localhost/test1", "root", "admin123");
         } catch (SQLException e) {
-            System.out.println("Ошибка подключения к БД");
             e.printStackTrace();
+            return null;
         }
-
-        return null;
     }
-
 }
